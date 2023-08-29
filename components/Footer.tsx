3e584b2c14ae910dfb646ca663aa7dc003dc1a4c
Sam Junior <79420880+samsul17-dv@users.noreply.github.com>
2023-08-30 00:35:33 +0700

    Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => (
       <div className='flex flex-col justify-start items-start gap-6'>
         <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain' />
         <p className='text-base text-gray-700'>
-          Carhub 2023 <br />
+          Aksara.art 2023 <br />
           All Rights Reserved &copy;
         </p>
       </div>
